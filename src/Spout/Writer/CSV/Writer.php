75d284eafad36df0593eb957ac4238e3fba057b9
Brendon Willian <bmota@its.jnj.com>
2017-02-16 10:35:28 -0200

    Fix

--- src/Spout/Writer/CSV/Writer.php
+++ src/Spout/Writer/CSV/Writer.php
@@ -95,8 +95,7 @@ class Writer extends AbstractWriter
     protected function addRowToWriter(array $dataRow, $style)
     {
         if($this->fieldEnclosure === null || $this->fieldEnclosure === '') {
-            $value = $this->lastWrittenRowIndex > 0 ? "\n". implode($this->fieldDelimiter, $dataRow) : implode($this->fieldDelimiter, $dataRow);
-            $wasWriteSuccessful = $this->globalFunctionsHelper->fputs($this->filePointer, $value);
+            $wasWriteSuccessful = $this->globalFunctionsHelper->fputs($this->filePointer, implode($this->fieldDelimiter, $dataRow) . "\n");
         } else {
             $wasWriteSuccessful = $this->globalFunctionsHelper->fputcsv($this->filePointer, $dataRow, $this->fieldDelimiter, $this->fieldEnclosure);
         }
